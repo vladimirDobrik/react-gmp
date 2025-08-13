d02a38be182f974bf7f5d5a44c55c4b0172e2465
Uladzimir Dobryk <uladzimir.dobryk@fsresidential.com>
2025-08-13 13:29:02 +0400

    init react app

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+
+
+function App(): React.JSX.Element {
+  return (
+    <div className="App"></div>
+  );
+}
+
+export default App;
---
+++ src/react-app-env.d.ts
@@ -0,0 +1,11 @@
+/// <reference types="react-scripts" />
+
+declare module "*.svg" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.css" {
+  const content: { [className: string]: string };
+  export default content;
+} 
