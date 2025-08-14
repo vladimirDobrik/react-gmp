10f4c67409ca25e036f4589c5384e3cbe8eaa583
Uladzimir Dobryk <uladzimir.dobryk@fsresidential.com>
2025-08-14 14:14:31 +0400

    feat: genre list

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css';
 import Search from './components/search/Search';
 import Counter from './components/counter/Counter';
-
+import Genres from './components/genres/Genres';
 
 function App(): React.JSX.Element {
   return (
     <div className="App">
       <Counter initialValue={0} />
       <Search />
+      <Genres/>
     </div>
   );
 }
